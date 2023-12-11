97db0f53998a9f2204eec962e26b7ba9c9d5151f
Kirsten Smith <kirstenalexis7@gmail.com>
2023-12-10 19:52:07 -0800

    WIP UI Logic

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,19 @@ Pizza.prototype.getPizzaCost = function () {
   let totalCost = baseCost + toppingCost + sizeCost;
   return totalCost.toFixed(2);
 };
+//UI Logic
+
+function pizzaFormHandler(e) {
+  e.preventDefault();
+  const selectedToppings = Array.from(document.querySelectorAll('input[name="topping"]:checked')).map(checkbox => checkbox.value);
+  const selectedSize = document.querySelector('input[name="size"]:checked');
+
+  const pizzaOrder = new Pizza(selectedToppings, selectedSize.value)
+  const cost = pizzaOrder.pizzaFormHandler();
+  document.getElementById("pizza-cost").textContent = `Total Cost: $${cost}`
+}
+
+
+window.addEventListener("load", function () {
+  document.querySelector("#form").addEventListener("submit", pizzaFormHandler)
+})
